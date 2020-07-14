f31c405454fe4defc1adda6aa4b3d2f9fe28c5e5
Michael Clark <33385949+michaelclark2@users.noreply.github.com>
2020-07-13 20:51:01 -0500

    Revert "added signin/out"

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,17 +1,2 @@
-import firebase from 'firebase/app';
-import apiKeys from './helpers/apiKeys.json';
-
-import auth from './components/auth/auth';
-import authData from './helpers/data/authData';
-
 import 'bootstrap';
 import '../stylings/main.scss';
-
-const init = () => {
-  firebase.initializeApp(apiKeys.firebaseConfig);
-  authData.checkLoginStatus();
-  auth.loginButton();
-  auth.logoutButton();
-};
-
-init();
