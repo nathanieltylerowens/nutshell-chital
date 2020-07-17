e96f02a4b3b608ed8e450ff5c748f6700d8c2f60
Austin Phy <atphy42@gmail.com>
2020-07-16 20:00:04 -0500

    printing class cards from firebase

--- src/javascripts/components/classes/buildClasses/buildClasses.js
+++ src/javascripts/components/classes/buildClasses/buildClasses.js
@@ -5,7 +5,19 @@ const getClasses = () => utils.readData('classes');
 const buildClasses = () => {
   getClasses()
     .then((classes) => {
-      console.warn('it works', classes);
+      let domString = '';
+      classes.forEach((eachClass) => {
+        domString += `
+          <div class="card" style="width: 18rem;">
+  <img src="${eachClass.imageUrl}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h1 class="card-title">${eachClass.name}</h1>
+    <p class="card-text">${eachClass.schedule}</p>
+  </div>
+</div>
+`;
+      });
+      utils.printToDom('#classes-container', domString);
     })
     .catch((err) => console.error('error retrieving class list', err));
 };
