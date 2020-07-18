a0fd6ab07ca7b817860c152c9eb7c50a4a8c522a
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-07-18 12:36:44 -0500

    add button is hidden on logout and showing on login same with student cards

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -1,17 +1,25 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
+import buildStudent from '../../components/students/buildStudent/buildStudent';
+import buildAuthStudent from '../../components/students/buildStudent/authBuildStudent';
+
 const logoutButton = $('#google-auth-out');
 const loginButton = $('#google-auth-in');
+const addButton = $('#addButton');
 
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       logoutButton.removeClass('hidden');
       loginButton.addClass('hidden');
+      addButton.removeClass('hidden');
+      buildStudent.buildStudents();
     } else {
       loginButton.removeClass('hidden');
       logoutButton.addClass('hidden');
+      addButton.addClass('hidden');
+      buildAuthStudent.buildAuthStudents();
     }
   });
 };
