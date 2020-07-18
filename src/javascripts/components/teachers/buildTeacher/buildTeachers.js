5222468d9cfda5b62d60f225f8576f6dba3bcf43
Olamide Oladinni <olajide1987@gmail.com>
2020-07-18 12:23:42 -0500

    cards print

---
+++ src/javascripts/components/teachers/buildTeacher/buildTeachers.js
@@ -0,0 +1,25 @@
+import utils from '../../../helpers/utils';
+
+const getTeachers = () => utils.readData('teachers');
+
+const buildTeachers = () => {
+  getTeachers()
+    .then((teachers) => {
+      let domString = '';
+      teachers.forEach((eachTeacher) => {
+        domString += `
+      <div class="card" style="width: 18rem;">
+        <img src="${eachTeacher.imageUrl}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h1 class="card-title">${eachTeacher.name}</h1>
+        <p class="card-text">${eachTeacher.Tenured}</p>
+      </div>
+      </div>
+      `;
+      });
+      utils.printToDom('#teachers-container', domString);
+    })
+    .catch((err) => console.error('error getting teachers', err));
+};
+
+export default { buildTeachers };
