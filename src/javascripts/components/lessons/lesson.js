ed46a00f850484b776f8ad4754edb80140b93a18
Joshua Medlen <joshua246@gmail.com>
2020-07-18 11:16:24 -0500

    changed get data to a global one

--- src/javascripts/components/lessons/lesson.js
+++ src/javascripts/components/lessons/lesson.js
@@ -4,12 +4,13 @@
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   </div>
 </div> */
-import lessonData from '../../helpers/data/lessonsData';
 import utils from '../../helpers/utils';
 import lessonsMakers from './lessonMaker';
 
+const getLessons = () => utils.readData('lessons');
+
 const lessonMaker = () => {
-  lessonData.getLessons()
+  getLessons()
     .then((lessons) => {
       let domString = '';
       lessons.forEach((lesson) => {
