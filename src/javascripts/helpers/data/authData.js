a0c19f6036ed1fd4adf412f8dba1257b1bfb10a7
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-07-18 13:46:15 -0500

    fixed the addButton show and hide

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -1,9 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-import buildStudent from '../../components/students/buildStudent/buildStudent';
-import buildAuthStudent from '../../components/students/buildStudent/authBuildStudent';
-
 const logoutButton = $('#google-auth-out');
 const loginButton = $('#google-auth-in');
 const addButton = $('#addButton');
@@ -14,12 +11,10 @@ const checkLoginStatus = () => {
       logoutButton.removeClass('hidden');
       loginButton.addClass('hidden');
       addButton.removeClass('hidden');
-      buildStudent.buildStudents();
     } else {
       loginButton.removeClass('hidden');
       logoutButton.addClass('hidden');
       addButton.addClass('hidden');
-      buildAuthStudent.buildAuthStudents();
     }
   });
 };
