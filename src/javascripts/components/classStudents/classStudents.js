0f267e16d78c36ed1487dea4b8d8de43b9d9f746
Nick Walters <nswalters.s@gmail.com>
2020-07-27 23:42:12 -0500

    WIP - function works to add classes which are new to the student (ie they've been enrolled). Next up will be to delete classes from a user which are now listed as available (either they never enrolled or have been unenrolled)

--- src/javascripts/components/classStudents/classStudents.js
+++ src/javascripts/components/classStudents/classStudents.js
@@ -1,1 +1,38 @@
 // Manage the classStudents join table
+import classStudentsData from '../../helpers/data/classStudents/classStudentsData';
+
+const modifyClassStudents = (studentId, addedClasses) => {
+  // if there are addedClasses
+  // then we need to check if a record exists for that student/class assignment already
+  // if there is, then we don't add it
+  if (addedClasses) {
+    classStudentsData.getClassStudentsByStudentId(studentId)
+      .then((classStudents) => {
+        const recordsFound = [];
+        const recordsNew = [];
+        addedClasses.forEach((addedClass) => {
+          console.error('AddedClass: ', addedClass);
+          const search = classStudents.find(((cs) => cs.classesId === addedClass));
+          if (search) {
+            recordsFound.push(search);
+          } else {
+            const newRecord = {
+              studentsId: studentId,
+              classesId: addedClass,
+            };
+            recordsNew.push(newRecord);
+          }
+        });
+        console.error('Records Found: ', recordsFound);
+        console.error('NEW Records Found: ', recordsNew);
+        recordsNew.forEach((record) => {
+          classStudentsData.addClassStudents(record);
+        });
+      })
+      .catch((err) => console.error('Could not getClassStudentsByStudentId', err));
+  }
+};
+
+export default {
+  modifyClassStudents,
+};
