d86045cc4bc5018367f946b327b8e07a5cd52a14
Michael Dotson <thedotson@icloud.com>
2020-07-28 18:47:21 -0500

    new master

--- src/javascripts/helpers/data/teacher/teacherClassData.js
+++ src/javascripts/helpers/data/teacher/teacherClassData.js
@@ -3,13 +3,8 @@ import apiKeys from '../../apiKeys.json';
 
 const baseUrl = apiKeys.firebaseConfig.databaseURL;
 
-const getTecherClassByTeacherId = (teacherId) => new Promise((resolve, reject) => {
-  axios.get(`${baseUrl}/classTeachers.json?orderBy="teachersId"&equalTo="${teacherId}"`)
-    .then((response) => {
-      const teacherClass = response.data;
-      resolve(teacherClass);
-    })
-    .catch((err) => reject(err));
+const getTeachersByClassId = (classId) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/classTeachers.json?orderBy="classesId"&equalTo="${classId}"`)
 });
 
 export default { getTecherClassByTeacherId };
